9b6df667ef5480e0910014ef412030bd32ba52db
Bryce Mullican <bmullican@madg.com>
2016-11-09 16:43:26 -0600

    minor code fix

--- src/jquery.mg-space.js
+++ src/jquery.mg-space.js
@@ -251,7 +251,7 @@
                 .addClass(_.stripDot(_.options.target)+'-open')
                 .css({
                     position: 'absolute',
-                    top: $('.mg-space').position().top + jQuery('.mg-space').parent().position().top,
+                    top: $('.mg-space').position().top + $('.mg-space').parent().position().top,
                     zIndex: 2,
                     paddingTop: _.options.targetPadding/2,
                     paddingBottom: _.options.targetPadding/2
